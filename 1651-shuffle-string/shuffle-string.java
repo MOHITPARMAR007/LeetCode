ee1fb83d8421fcc26d454f08883afb8fafb5b740
mohit_parmar_007 <105337843+MOHITPARMAR007@users.noreply.github.com>
2024-06-27 18:36:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (66.93%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -1,18 +1,8 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        // Initialize a StringBuilder with the length of the input string
-        StringBuilder finalString = new StringBuilder(s.length());
-
-        // Initialize the StringBuilder with spaces or any placeholder character
-        for (int i = 0; i < s.length(); i++) {
-            finalString.append(' ');
-        }
-
-        // Place each character at the correct position
-        for (int i = 0; i < indices.length; i++) {
-            finalString.setCharAt(indices[i], s.charAt(i));
-        }
-
-        return finalString.toString();
+       char[] arr = new char[s.length()];
+       for(int i=0;i<s.length();i++)
+            arr[indices[i]] = s.charAt(i);
+       return String.valueOf(arr);
     }
 }
