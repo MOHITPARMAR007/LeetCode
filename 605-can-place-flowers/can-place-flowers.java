813349f2c13be46dfdd3b200d2bb607c8a20a14f
mohit_parmar_007 <105337843+MOHITPARMAR007@users.noreply.github.com>
2024-12-24 16:35:15 +0530

    Time: 1 ms (96.84%) | Memory: 45.7 MB (47.02%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -2,19 +2,20 @@ class Solution {
     public boolean canPlaceFlowers(int[] fls, int n) {
         int count = 0;
         int len = fls.length;
-        
+
+        if (n == 0) {
+            return true;
+        }
+
         for (int i = 0; i < len; i++) {
-            // Check if current plot is empty and neighbors (if exist) are also empty
             if (fls[i] == 0 && (i == 0 || fls[i - 1] == 0) && (i == len - 1 || fls[i + 1] == 0)) {
-                fls[i] = 1; // Plant a flower
-                count++; // Increment count of planted flowers
-                
-                if (count >= n) {
-                    return true; // Return true if required flowers are planted
+                fls[i] = 1; // Mark the spot as planted
+                count++;
+                if (count == n) {
+                    return true;
                 }
-                i++; // Skip the next plot
             }
         }
-        return count >= n; // Check if enough flowers were planted
+        return false;
     }
 }
