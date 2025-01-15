d34c4ab226a7bdd6f128fb068c0a83435893f132
mohit_parmar_007 <105337843+MOHITPARMAR007@users.noreply.github.com>
2025-01-16 00:13:34 +0530

    Time: 1 ms (55.22%) | Memory: 43 MB (17.46%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,25 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
-
     public List<List<Integer>> subsets(int[] nums) {
-        helper(new ArrayList<>(), nums, 0);
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        int n = nums.length;
+        
+        recursion(0, curr, nums, res);
         return res;
     }
-
-    private void helper(List<Integer> current, int[] nums, int index) {
-        if (index == nums.length) {
-            res.add(new ArrayList<>(current));
-            return;
+    
+    private void recursion(int cur_index, List<Integer> curr, int[] nums, List<List<Integer>> res) {
+        res.add(new ArrayList<>(curr));
+        
+        for (int index_of_next = cur_index; index_of_next < nums.length; index_of_next++) {
+            curr.add(nums[index_of_next]);
+            recursion(index_of_next + 1, curr, nums, res);
+            curr.remove(curr.size() - 1);
         }
-
-        helper(current, nums, index + 1);
-
-        current.add(nums[index]);
-        helper(current, nums, index + 1);
-
-        current.remove(current.size() - 1);
     }
 }
